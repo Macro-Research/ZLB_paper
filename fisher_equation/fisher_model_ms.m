5b2f83e3238826bb97472161813fd58d9c2d0f80
Tolga Ozden <tolgaozdn@gmail.com>
2018-11-06 16:56:28 +0100

    update

--- fisher_equation/fisher_model_ms.m
+++ fisher_equation/fisher_model_ms.m
@@ -1,13 +1,13 @@
 clear;clc;%close all;
 %Fisher equation with two (Markov) regimes, msv learning. 
-%rng(1);
-N=10000;
-phi_pinf1=5;
-phi_pinf2=.5;
+%rng(2);
+N=1000;
+phi_pinf1=.899;
+phi_pinf2=2;
 rho=0.9;
 eta_sigma=0.1;
 eta=normrnd(0,eta_sigma,[N 1]);
-p_11=0.99;p_22=0.9;
+p_11=0.95;p_22=0.95;
 Q=[p_11,1-p_11;1-p_22,p_22];
 regime=zeros(N,1);
 regime(1)=1;
@@ -46,17 +46,17 @@ rr_tt=eye(2);
 forecast_errors=zeros(N,1);
 forecast=zeros(N,1);
 for jj=2:N
-    gain=0.001;
+    gain=0.05;
     regime(jj)=findRegime(regime(jj-1),p_11,p_22);
     r(jj)=rho*r(jj-1)+eta(jj);
      learning(jj,:)=[alpha_tt,beta_tt];%forecast coef for t
      learningCovariance(jj,:,:)=rr_tt;
     
      forecast_errors(jj-1)=pinf(jj-1)-alpha_tt+beta_tt*r(jj-1);
-     if jj>10
+%      if jj>10
    [alpha_tt beta_tt rr_tt] =...
        l_LS(pinf(jj-1),[1 r(jj-1)]',alpha_tt,beta_tt,rr_tt,gain);%forecast for t+1
-     end
+%      end
     pinf(jj)=(regime(jj)*(1/phi_pinf1)+(1-regime(jj))*(1/phi_pinf2))...
     *((alpha_tt+beta_tt*rho*r(jj))+r(jj));
 
@@ -65,7 +65,8 @@ for jj=2:N
    
 end
 
-figure;
+figure('Name','Fisher eqn-learning coef','units','normalized','outerposition',[0 0 1 1]);
+
 % subplot(3,1,1);
 % plot(learning(:,1),'lineWidth',3);
 % hold on;
@@ -75,8 +76,8 @@ subplot(2,1,1);
 plot(learning(:,2),'lineWidth',3);
 hold on;
 plot(ones(N,1)*beta_ree,'--');
-hold on;
-plot(ones(N,1)*(1/(phi_pinf1-rho)),'--');
+%hold on;
+%plot(ones(N,1)*(1/(phi_pinf1-rho)),'--');
 hold on;
 plot(ones(N,1)*(1/(phi_pinf2-rho)),'--');
 legend('learning coef','unconditional','regime 1','regime 2');
@@ -89,32 +90,49 @@ area(regime);
 % plot(forecast_errors,'lineWidth',3);
 title('regime');
 
-figure;
-subplot(2,1,1);
-plot(forecast_errors,'lineWidth',3);
-title('forecast errors');
-subplot(2,1,2);
-area(regime);
-title('regime');
+fig = gcf;
+fig.PaperPositionMode = 'auto'
+fig_pos = fig.PaperPosition;
+fig.PaperSize = [fig_pos(3) fig_pos(4)];
+print(fig,'fisher_simulation2_learningCoef','-dpdf'); 
 
-figure;
-subplot(2,1,1);
+figure('Name','simulated inflation','units','normalized','outerposition',[0 0 1 1]);
 plot(pinf,'lineWidth',3);
-title('Inflation');
-subplot(2,1,2);
-area(regime);
-title('regime');
+
+
+fig = gcf;
+fig.PaperPositionMode = 'auto'
+fig_pos = fig.PaperPosition;
+fig.PaperSize = [fig_pos(3) fig_pos(4)];
+print(fig,'fisher_simulation2_pinf','-dpdf'); 
+
+
+% figure;
+% subplot(2,1,1);
+% plot(forecast_errors,'lineWidth',3);
+% title('forecast errors');
+% subplot(2,1,2);
+% area(regime);
+% title('regime');
+% 
+% figure;
+% subplot(2,1,1);
+% plot(pinf,'lineWidth',3);
+% title('Inflation');
+% subplot(2,1,2);
+% area(regime);
+% title('regime');
 % figure;
 % plotyy(linspace(0,N,N),regime,...
 %     linspace(0,N,N),forecast_errors);
 % title('forecast errors');
 
-figure;
-subplot(2,2,1);
-plot(learningCovariance(:,1,1),'lineWidth',3);
-subplot(2,2,2);
-plot(learningCovariance(:,1,2),'lineWidth',3);
-subplot(2,2,3);
-plot(learningCovariance(:,2,1),'lineWidth',3);
-subplot(2,2,4);
-plot(learningCovariance(:,2,2),'lineWidth',3);
+% figure;
+% subplot(2,2,1);
+% plot(learningCovariance(:,1,1),'lineWidth',3);
+% subplot(2,2,2);
+% plot(learningCovariance(:,1,2),'lineWidth',3);
+% subplot(2,2,3);
+% plot(learningCovariance(:,2,1),'lineWidth',3);
+% subplot(2,2,4);
+% plot(learningCovariance(:,2,2),'lineWidth',3);
---
+++ latex_files/startup.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Tolga\Desktop\zlb_paper\latex_files
+
+"adaptive learning with occasionally binding constraints.tex" -stylesheet /mystyle.css
